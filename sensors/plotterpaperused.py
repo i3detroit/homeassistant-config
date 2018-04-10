568c31225aa0d0cd4314e4dc8c714fff2edec060
mkfink <mkfink@gmail.com>
2018-04-09 22:26:42 -0400

    added plotter paper and ink lifetime usage sensors

---
+++ sensors/plotterpaperused.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+html = requests.get("http://10.13.0.52/hp/device/webAccess/index.htm;jsessionid=r0zrwjx0r1?content=usage").text
+
+soup = BeautifulSoup(html, "lxml")
+table = soup.find("table", attrs={"class":"tablePad3"})
+
+# The first tr contains the field names.
+
+rows = []
+for row in table.find_all("tr")[0:]:
+    row = (td.get_text() for td in row.find_all("td"))
+    rows.append(list(row))
+
+out = (re.sub("[^\d.]+", "", rows[0][1]))
+print(out)
